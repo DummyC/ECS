b5737e2f97b4cb803133fa38cf87426b42be053c
DummyCoding <modestopellerin@gmail.com>
2025-05-10 22:43:38 +0800

    Adjust logo size in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
             <div
                 class="bg-[#d2d7ff] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                 <img src="{{ asset('images/ecs_logo.png') }}" alt="ECS Logo"
-                    class="absolute inset-0 w-full h-full object-cover object-center" loading="lazy" />
+                    class="absolute inset-0 w-full h-full object-cover object-center scale-90" loading="lazy" />
                 <div
                     class="absolute inset-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)]">
                 </div>
